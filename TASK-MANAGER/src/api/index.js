c05e96fdb239b35900a1d7d2f23efebd10d7580b
Uzer Qureshi <121750611+08Uzair@users.noreply.github.com>
2026-05-11 10:13:17 +0530

    updated API Link

--- TASK-MANAGER/src/api/index.js
+++ TASK-MANAGER/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://uzerqureshi-devtown-assignment-serv.vercel.app/api/v1/",
 });
 
 API.interceptors.request.use((req) => {
